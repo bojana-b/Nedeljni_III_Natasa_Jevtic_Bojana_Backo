f7953a2aeb9b9b093d4ab094851d7898a81d1c29
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-08-20 11:17:29 +0200

    Update UserView

--- Nedeljni_III_Natasa_Jevtic_Bojana_Backo/Nedeljni_III_Natasa_Jevtic_Bojana_Backo/View/UserView.xaml.cs
+++ Nedeljni_III_Natasa_Jevtic_Bojana_Backo/Nedeljni_III_Natasa_Jevtic_Bojana_Backo/View/UserView.xaml.cs
@@ -2,8 +2,12 @@
 using Nedeljni_III_Natasa_Jevtic_Bojana_Backo.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace Nedeljni_III_Natasa_Jevtic_Bojana_Backo.View
 {
@@ -12,6 +16,8 @@ namespace Nedeljni_III_Natasa_Jevtic_Bojana_Backo.View
     /// </summary>
     public partial class UserView : Window
     {
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
         public UserView(vwUser userLogged)
         {
             InitializeComponent();
@@ -37,6 +43,64 @@ namespace Nedeljni_III_Natasa_Jevtic_Bojana_Backo.View
         private void txtFilter_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
             CollectionViewSource.GetDefaultView(lvUsers.ItemsSource).Refresh();
+        }
+
+        private void lvUsersColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                lvUsers.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            lvUsers.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+    }
+    public class SortAdorner : Adorner
+    {
+        private static Geometry ascGeometry =
+            Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z");
+
+        private static Geometry descGeometry =
+            Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
+
+        public ListSortDirection Direction { get; private set; }
+
+        public SortAdorner(UIElement element, ListSortDirection dir)
+            : base(element)
+        {
+            this.Direction = dir;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            if (AdornedElement.RenderSize.Width < 20)
+                return;
+
+            TranslateTransform transform = new TranslateTransform
+                (
+                    AdornedElement.RenderSize.Width - 15,
+                    (AdornedElement.RenderSize.Height - 5) / 2
+                );
+            drawingContext.PushTransform(transform);
+
+            Geometry geometry = ascGeometry;
+            if (this.Direction == ListSortDirection.Descending)
+                geometry = descGeometry;
+            drawingContext.DrawGeometry(Brushes.Black, null, geometry);
+
+            drawingContext.Pop();
         }
     }
 }
