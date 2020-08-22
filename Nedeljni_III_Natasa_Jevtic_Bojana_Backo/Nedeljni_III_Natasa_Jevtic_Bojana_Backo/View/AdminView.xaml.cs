c6ba5afad2169803a28fc17ccef776e716668b55
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-08-22 18:38:38 +0200

    Update AdmivView

--- Nedeljni_III_Natasa_Jevtic_Bojana_Backo/Nedeljni_III_Natasa_Jevtic_Bojana_Backo/View/AdminView.xaml.cs
+++ Nedeljni_III_Natasa_Jevtic_Bojana_Backo/Nedeljni_III_Natasa_Jevtic_Bojana_Backo/View/AdminView.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Nedeljni_III_Natasa_Jevtic_Bojana_Backo.ViewModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
 
 namespace Nedeljni_III_Natasa_Jevtic_Bojana_Backo.View
 {
@@ -12,6 +15,16 @@ namespace Nedeljni_III_Natasa_Jevtic_Bojana_Backo.View
         {
             InitializeComponent();
             this.DataContext = new AdminViewModel(this);
+        }
+        protected void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var view = CollectionViewSource.GetDefaultView((DataContext as AdminViewModel).RecipeList);
+            view.Filter = o => (o as vwRecipe).RecipeName.Contains((sender as TextBox).Text);
+        }
+        protected void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var view = CollectionViewSource.GetDefaultView((DataContext as AdminViewModel).RecipeList);
+            view.Filter = o => (o as vwRecipe).Type.Contains((sender as TextBox).Text);
         }
     }
 }
